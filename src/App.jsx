c3634314842ff423d529060e357d41b1dcfe6573
mh1k3code <56066204+mh1k3code@users.noreply.github.com>
2025-11-24 04:01:14 +0200

    Update project with new 3d Features

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import NavBar from "./components/NavBar";
 import ProductViewer from "./components/ProductViewer";
 import gsap from "gsap";
 import {ScrollTrigger } from "gsap/ScrollTrigger";
+import Showcase from "./components/Showcase";
+import Performance from "./components/Performance";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
+
 
 gsap.registerPlugin(ScrollTrigger )
 
@@ -14,6 +20,11 @@ function App (){
        <NavBar/>
        <Hero/>
        <ProductViewer/>
+       <Showcase/>
+       <Performance/>
+       <Features/>
+       <Highlights/>
+       <Footer/>
       </>
     
     );
